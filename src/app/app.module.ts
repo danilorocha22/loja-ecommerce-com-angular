186545e926004d10d19371f3e584feb4f5ebd92a
danilorocha22 <danilo.rochaa@gmail.com>
2022-09-14 09:46:35 -0300

    implementação do serviço de notificação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { registerLocaleData } from '@angular/common';
 import ptBr from '@angular/common/locales/pt';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PaginaNaoEncontradaComponent } from './components/pagina-nao-encontrada/pagina-nao-encontrada.component';
+/* ************************************************************************************************************ */
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 registerLocaleData(ptBr);
 
@@ -20,7 +23,9 @@ registerLocaleData(ptBr);
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' }
